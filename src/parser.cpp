e552b069a038eb09d10da13c7c1dbab34d6cf219
Yaram <pootisspencerherenao@gmail.com>
2019-06-10 23:49:17 +1000

    Fix small error in function type parsing

--- src/parser.cpp
+++ src/parser.cpp
@@ -741,6 +741,8 @@ static Result<Expression> parse_right_expressions(Context *context, List<Operati
 
                         skip_whitespace(context);
 
+                        auto character = fgetc(context->source_file);
+
                         Expression *return_type;
                         if(character == '-') {
                             context->character += 1;
@@ -759,6 +761,8 @@ static Result<Expression> parse_right_expressions(Context *context, List<Operati
 
                             return_type = heapify(result.value);
                         } else {
+                            ungetc(character, context->source_file);
+
                             return_type = nullptr;
                         }
 
