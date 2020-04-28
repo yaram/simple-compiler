2fd0d7427978e7bb80dc0e54997ac8f08e0ef2f8
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-04-29 09:13:22 +1000

    Modify profiler to output speedscope profiles

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -13,39 +13,64 @@ void init_profiler() {
     profiler_buffer_pointer = profiler_buffer;
 }
 
-// Output collapsed stack traces suitable for https://github.com/brendangregg/FlameGraph
+struct SpeedscopeEntry {
+    size_t type;
 
-static void dump_stack_frame(FILE *file, List<const char*> *stack, size_t *index, size_t performance_counter) {
+    uint64_t time;
+
+    bool is_exit;
+};
+
+static void process_stack_frame_for_speedscope(List<const char*> *type_names, List<SpeedscopeEntry> *entries, size_t *index) {
+    assert(profiler_buffer[*index] == 0);
+
+    auto function_name = *(const char**)&profiler_buffer[*index + 1];
+
+    auto performance_counter = *(uint64_t*)&profiler_buffer[*index + 1 + sizeof(const char*)];
+
+    *index += 1 + sizeof(const char*) + sizeof(uint64_t);
+
+    auto found_type = false;
+    size_t stack_frame_type;
+    for(size_t i = 0; i < type_names->count; i += 1) {
+        auto name = (*type_names)[i];
+
+        if(name == function_name) {
+            found_type = true;
+            stack_frame_type = i;
+
+            break;
+        }
+    }
+
+    if(!found_type) {
+        stack_frame_type = append(type_names, function_name);
+    }
+
+    append(entries, {
+        stack_frame_type,
+        performance_counter,
+        false
+    });
+
     while(true) {
-        assert(stack->count != 0);
-
         auto buffer_used = (size_t)profiler_buffer_pointer - (size_t)profiler_buffer;
         assert(*index < buffer_used);
 
         if(profiler_buffer[*index] == 0) {
-            auto function_name = *(const char**)&profiler_buffer[*index + 1];
-
-            auto performance_counter = *(uint64_t*)&profiler_buffer[*index + 1 + sizeof(const char*)];
-
-            *index += 1 + sizeof(const char*) + sizeof(uint64_t);
-
-            append(stack, function_name);
-
-            dump_stack_frame(file, stack, index, performance_counter);
-
-            stack->count -= 1;
+            process_stack_frame_for_speedscope(type_names, entries, index);
         } else {
             assert(profiler_buffer[*index] == 1);
 
-            auto end_performance_counter = *(uint64_t*)&profiler_buffer[*index + 1];
+            auto performance_counter = *(uint64_t*)&profiler_buffer[*index + 1];
 
             *index += 1 + sizeof(uint64_t);
 
-            for(auto stack_frame : *stack) {
-                fprintf(file, "%s;", stack_frame);
-            }
-
-            fprintf(file, " %llu\n", end_performance_counter - performance_counter);
+            append(entries, {
+                stack_frame_type,
+                performance_counter,
+                true
+            });
 
             return;
         }
@@ -55,10 +80,8 @@ static void dump_stack_frame(FILE *file, List<const char*> *stack, size_t *index
 void dump_profile() {
     printf("Generating profiler dump...\n");
 
-    auto file = fopen("collapsed_stacks.txt", "w");
-    assert(file);
-
-    List<const char*> stack {};
+    List<const char*> type_names {};
+    List<SpeedscopeEntry> entries {};
 
     size_t index = 0;
 
@@ -66,20 +89,49 @@ void dump_profile() {
 
     while(index < buffer_used) {
         assert(profiler_buffer[index] == 0);
-        assert(stack.count == 0);
 
-        auto function_name = *(const char**)&profiler_buffer[index + 1];
-
-        auto performance_counter = *(uint64_t*)&profiler_buffer[index + 1 + sizeof(const char*)];
-
-        index += 1 + sizeof(const char*) + sizeof(uint64_t);
-
-        append(&stack, function_name);
-
-        dump_stack_frame(file, &stack, &index, performance_counter);
-
-        stack.count -= 1;
+        process_stack_frame_for_speedscope(&type_names, &entries, &index);
     }
+
+    auto file = fopen("simple-compiler.speedscope.json", "w");
+    assert(file);
+
+    fprintf(file, "{\"version\":\"0.0.1\",\"$schema\":\"https://www.speedscope.app/file-format-schema.json\",\"shared\":{\"frames\":[");
+
+    for(size_t i = 0; i < type_names.count; i += 1) {
+        auto name = type_names[i];
+
+        fprintf(file, "{\"name\":\"%s\"}", name);
+
+        if(i != type_names.count - 1) {
+            fprintf(file, ",");
+        }
+    }
+
+    fprintf(
+        file,
+        "]},\"profiles\":[{\"type\":\"evented\",\"name\":\"simple-compiler\",\"unit\":\"none\",\"startValue\":%llu,\"endValue\":%llu,\"events\":[",
+        entries[0].time,
+        entries[entries.count - 1].time
+    );
+
+    for(size_t i = 0; i < entries.count; i += 1) {
+        auto frame_entry = entries[i];
+
+        if(frame_entry.is_exit) {
+            fprintf(file, "{\"type\":\"C\",\"frame\":%zu,\"at\":%llu}", frame_entry.type, frame_entry.time);
+        } else {
+            fprintf(file, "{\"type\":\"O\",\"frame\":%zu,\"at\":%llu}", frame_entry.type, frame_entry.time);
+        }
+
+        if(i != entries.count - 1) {
+            fprintf(file, ",");
+        }
+    }
+
+    fprintf(file, "]}]}");
+
+    fclose(file);
 
     printf("Done!\n");
     printf("Profiler buffer use: %.2f%%\n", (double)buffer_used / profiler_buffer_size * 100);
