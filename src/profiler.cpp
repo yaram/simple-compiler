61ce46f425912ec0d4eb4ea5a10aecccd71e0c47
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-04-29 08:39:07 +1000

    Modify profiler to output FlameGraph collapsed stack traces

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1,7 +1,7 @@
 #include "profiler.h"
 #include <stdio.h>
-#include <string.h>
-#include "platform.h"
+#include <assert.h>
+#include "list.h"
 
 // Allocate 10MB of .bss space for the profiler record buffer
 const size_t profiler_buffer_size = 1024 * 1024 * 10;
@@ -9,76 +9,43 @@ uint8_t profiler_buffer[profiler_buffer_size];
 
 uint8_t *profiler_buffer_pointer;
 
-bool read_performance_frequency;
-uint64_t performance_frequency;
-
-
-#if defined(OS_WINDOWS)
-
-#include <Windows.h>
-
 void init_profiler() {
     profiler_buffer_pointer = profiler_buffer;
-
-    read_performance_frequency = false;
-
-    HKEY key;
-    if(RegOpenKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0, KEY_READ, &key) == ERROR_SUCCESS) {
-        DWORD type;
-        DWORD value;
-        DWORD dword_size = sizeof(DWORD);
-        if(
-            RegQueryValueExA(key, "~MHz", 0, &type, (LPBYTE)&value, &dword_size) == ERROR_SUCCESS &&
-            type == REG_DWORD
-        ) {
-            read_performance_frequency = true;
-            performance_frequency = (uint64_t)value * 1000000;
-        }
-    }
 }
 
-#endif
+// Output collapsed stack traces suitable for https://github.com/brendangregg/FlameGraph
 
-static void dump_profile_internal(uint8_t **pointer, unsigned int indentation_level, uint64_t entry_performance_counter) {
-    while((*pointer) < profiler_buffer_pointer) {
-        if((*pointer)[0] == 0) {
-            auto function_name = *(const char**)&(*pointer)[1];
+static void dump_stack_frame(FILE *file, List<const char*> *stack, size_t *index, size_t performance_counter) {
+    while(true) {
+        assert(stack->count != 0);
 
-            auto performance_counter = *(uint64_t*)&(*pointer)[1 + sizeof(const char*)];
+        auto buffer_used = (size_t)profiler_buffer_pointer - (size_t)profiler_buffer;
+        assert(*index < buffer_used);
 
-            for(unsigned int i = 0; i < indentation_level; i += 1) {
-                printf(" ");
-            }
+        if(profiler_buffer[*index] == 0) {
+            auto function_name = *(const char**)&profiler_buffer[*index + 1];
 
-            printf("%s\n", function_name);
+            auto performance_counter = *(uint64_t*)&profiler_buffer[*index + 1 + sizeof(const char*)];
 
-            *pointer = &(*pointer)[1 + sizeof(const char*) + sizeof(uint64_t)];
+            *index += 1 + sizeof(const char*) + sizeof(uint64_t);
 
-            dump_profile_internal(pointer, indentation_level + 1, performance_counter);
+            append(stack, function_name);
+
+            dump_stack_frame(file, stack, index, performance_counter);
+
+            stack->count -= 1;
         } else {
-            auto performance_counter = *(uint64_t*)&(*pointer)[1];
+            assert(profiler_buffer[*index] == 1);
 
-            *pointer = &(*pointer)[1 + sizeof(uint64_t)];
+            auto end_performance_counter = *(uint64_t*)&profiler_buffer[*index + 1];
 
-            for(unsigned int i = 0; i < indentation_level - 1; i += 1) {
-                printf(" ");
+            *index += 1 + sizeof(uint64_t);
+
+            for(auto stack_frame : *stack) {
+                fprintf(file, "%s;", stack_frame);
             }
 
-            auto ticks = performance_counter - entry_performance_counter;
-
-            if(read_performance_frequency) {
-                if(ticks < performance_frequency / 1000000) {
-                    printf("%f ns\n",  (double)ticks / performance_frequency * 1000000000);
-                } else if(ticks < performance_frequency / 1000) {
-                    printf("%f us\n",  (double)ticks / performance_frequency * 1000000);
-                } else if(ticks < performance_frequency) {
-                    printf("%f ms\n",  (double)ticks / performance_frequency * 1000);
-                } else {
-                    printf("%f s\n",  (double)ticks / performance_frequency);
-                }
-            } else {
-                printf("%llu clocks\n", ticks);
-            }
+            fprintf(file, " %llu\n", end_performance_counter - performance_counter);
 
             return;
         }
@@ -86,21 +53,34 @@ static void dump_profile_internal(uint8_t **pointer, unsigned int indentation_le
 }
 
 void dump_profile() {
-    auto pointer = profiler_buffer;
+    printf("Generating profiler dump...\n");
 
-    while(pointer < profiler_buffer_pointer) {
-        auto function_name = *(const char**)&pointer[1];
+    auto file = fopen("collapsed_stacks.txt", "w");
+    assert(file);
 
-        auto performance_counter = *(uint64_t*)&pointer[1 + sizeof(const char*)];
+    List<const char*> stack {};
 
-        printf("%s\n", function_name);
-
-        pointer = &pointer[1 + sizeof(const char*) + sizeof(uint64_t)];
-
-        dump_profile_internal(&pointer, 1, performance_counter);
-    }
+    size_t index = 0;
 
     auto buffer_used = (size_t)profiler_buffer_pointer - (size_t)profiler_buffer;
 
+    while(index < buffer_used) {
+        assert(profiler_buffer[index] == 0);
+        assert(stack.count == 0);
+
+        auto function_name = *(const char**)&profiler_buffer[index + 1];
+
+        auto performance_counter = *(uint64_t*)&profiler_buffer[index + 1 + sizeof(const char*)];
+
+        index += 1 + sizeof(const char*) + sizeof(uint64_t);
+
+        append(&stack, function_name);
+
+        dump_stack_frame(file, &stack, &index, performance_counter);
+
+        stack.count -= 1;
+    }
+
+    printf("Done!\n");
     printf("Profiler buffer use: %.2f%%\n", (double)buffer_used / profiler_buffer_size * 100);
 }
