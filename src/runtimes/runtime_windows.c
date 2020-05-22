ab5eea6ba616e3d14dd6563207e448700bec40b7
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-05-22 20:04:51 +1000

    Add stack alignment asm to windows runtime

--- src/runtimes/runtime_windows.c
+++ src/runtimes/runtime_windows.c
@@ -3,6 +3,8 @@ int MAIN();
 void __stdcall ExitProcess(unsigned int uExitCode);
 
 void __stdcall entry() {
+    asm("andq $-16, %rsp"); // Align stack to 16-byte boundaries for SSE to avoid segmentation fault
+
     int result = MAIN();
 
     ExitProcess(result);
